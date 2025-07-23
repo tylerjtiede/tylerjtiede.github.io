3965ef13a903597816cf654894b5c2f6de76e7c5
Tyler Tiede <tylertiede@gmail.com>
2025-07-23 14:59:17 -0400

    fixing errors

--- scripts/app.py
+++ scripts/app.py
@@ -2,8 +2,10 @@
 
 from flask import Flask, request, jsonify
 from rating_calculator import calculate_rating
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/api/calculate', methods=['GET'])
 def calculate():
