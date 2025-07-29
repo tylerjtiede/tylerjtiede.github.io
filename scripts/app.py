d6a643d846c3c6e4bc28de5a4b0bb83ba522c2a6
Tyler Tiede <tylertiede@gmail.com>
2025-07-29 10:59:37 -0400

    updated url

--- scripts/app.py
+++ scripts/app.py
@@ -5,7 +5,7 @@ from rating_calculator import calculate_rating
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, origins=["https://tylerjtiede.github.io"])
+CORS(app, origins=["https://www.tylertiede.com"])
 
 @app.route('/api/calculate', methods=['GET'])
 def calculate():
