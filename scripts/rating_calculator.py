7202dc2c7a4185aa9e7a00d10246c4c9e1ff225c
Tyler Tiede <tylertiede@gmail.com>
2025-08-04 19:20:05 -0400

    fixed bug with DNFs

--- scripts/rating_calculator.py
+++ scripts/rating_calculator.py
@@ -190,10 +190,15 @@ def calculate_rating(pdga_number, whatif=None):
     new_tournaments = [t for t in tournaments_stats if t['link'] not in tournament_ids]
 
     additional_rounds = []
+    tournaments_to_remove = [] # for DNFs
     for tournament in new_tournaments:
         link = tournament['link']
 
         ratings, timestamp, date, league = get_ratings_from_tournament_page(link)
+
+        if not ratings:
+            tournaments_to_remove.append(tournament)
+            continue
 
         for i, rating in enumerate(ratings):
             if i==0:
@@ -204,6 +209,10 @@ def calculate_rating(pdga_number, whatif=None):
                 tournament_copy['rating'] = int(ratings[i])
                 tournament_copy['round'] = i+1
                 additional_rounds.append(tournament_copy)
+
+    # remove tournaments with no rating
+    for tournament in tournaments_to_remove:
+        new_tournaments.remove(tournament)
 
     new_tournaments.extend(additional_rounds)
 
